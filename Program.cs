021890c9a583c2b648af1bea705128edf2c7bce9
Charlie Herndon <charlie.herndon@outlook.com>
2025-08-28 12:42:23 -0500

    Initial ChGit console app setup

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: chgit <command> [options]");
+                return;
+            }
+
+            string command = args[0].ToLower();
+
+            switch (command)
+            {
+                case "init":
+                    InitRepo();
+                    break;
+
+                default:
+                    Console.WriteLine($"Unknown command: {command}");
+                    break;
+            }
+        }
+
+        static void InitRepo()
+        {
+            Console.WriteLine("Init command called (not implemented yet).");
+        }
+    }
+}
